8977ae5ac4adb3851b5ccc7cda63c71f5b64c723
vadnov <vadnov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-05 14:45:56 +0000

    Linkwithin fix
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@676476 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atcontent.php
+++ atcontent.php
@@ -3,12 +3,12 @@
     Plugin Name: AtContent
     Plugin URI: http://atcontent.com/
     Description: Why 5000 Sites Have Chosen AtContent? Because it’s the easiest way to Reach new readership & Increase search ranking!
-    Version: 1.7.31
+    Version: 1.7.32
     Author: AtContent, IFFace, Inc.
     Author URI: http://atcontent.com/
     */
 
-    define( 'AC_VERSION', "1.7.31" );
+    define( 'AC_VERSION', "1.7.32" );
     define( 'AC_NO_PROCESS_EXCERPT_DEFAULT', "1" );
 
     require_once("atcontent_api.php");
@@ -582,10 +582,10 @@ END;
         //end EmbedPlus fix
 
         //linkwithin
-        try {
+        if ( function_exists( "linkwithin_add_hook" ) ) {
             remove_filter( 'the_excerpt', 'linkwithin_display_excerpt' );
             remove_filter( 'the_content', 'linkwithin_add_hook' );
-        } catch (Exception $e) { }
+        }
         //end linkwithin
     }
 
