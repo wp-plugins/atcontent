3e8bb7ee80c5ed9f6cefd484fa197ecf48f3fe7c
vadnov <vadnov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-08 08:32:06 +0000

    Plugin version fix
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@737642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atcontent.php
+++ atcontent.php
@@ -3,12 +3,12 @@
     Plugin Name: AtContent
     Plugin URI: http://atcontent.com/
     Description: Why 3,500 Sites Have Chosen AtContent? Because it’s the easiest way to Reach new readership & Increase search ranking!
-    Version: 2.5.1
+    Version: 2.5.2
     Author: AtContent, IFFace, Inc.
     Author URI: http://atcontent.com/
     */
 
-    define( 'AC_VERSION', "2.5.0.42" );
+    define( 'AC_VERSION', "2.5.2.44" );
     define( 'AC_NO_PROCESS_EXCERPT_DEFAULT', "1" );
 
     require_once( "atcontent_api.php" );
