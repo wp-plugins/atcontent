46d2bfdf3a8b69fdde8a0719f34cf4fcc838f837
vadnov <vadnov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-30 17:39:20 +0000

    special fixes for billbelew.com
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@780459 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atcontent.php
+++ atcontent.php
@@ -3,12 +3,12 @@
     Plugin Name: AtContent
     Plugin URI: http://atcontent.com/
     Description: Why 3,500 Sites Have Chosen AtContent? Because it’s the easiest way to Reach new readership & Increase search ranking!
-    Version: 4.2.8
+    Version: 4.2.9
     Author: AtContent, IFFace, Inc.
     Author URI: http://atcontent.com/
     */
 
-    define( 'AC_VERSION', "4.2.8.83" );
+    define( 'AC_VERSION', "4.2.9.84" );
     define( 'AC_NO_PROCESS_EXCERPT_DEFAULT', "1" );
 
     require_once( "atcontent_api.php" );
@@ -861,6 +861,7 @@ function ACgetCookie(name) {
 ?>
 <script type="text/javascript">
 jQuery(document).ready( function($) {
+    if (window.location.href.indexOf("billbelew.com") != -1) return;
     if (ACgetCookie("ac-connect-dismiss") != "1") {
         $('#toplevel_page_atcontent-settings').pointer({
             content: '<?php echo $pointer_content; ?>',
@@ -882,6 +883,7 @@ jQuery(document).ready( function($) {
 ?>
 <script type="text/javascript">
 jQuery(document).ready( function($) {
+    if (window.location.href.indexOf("billbelew.com") != -1) return;
     if (ACgetCookie("ac-location-dismiss") != "1") {
         jQuery('#toplevel_page_atcontent-settings').pointer({
             content: '<?php echo $pointer_content; ?>',
