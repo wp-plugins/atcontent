9b6cb65609083a6718ae6d51b78b1da274666b6d
opuramu <opuramu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-07 12:20:22 +0000

    connect page update
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@834323 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- invite.php
+++ invite.php
@@ -6,8 +6,8 @@
 <form id="connect_form" method="post" action="<?php echo $form_action; ?>">
     <input type="hidden" name="atcontent_invite" value="Y">
 <div class="atcontent_invite">
-    <h1>AtContent is the easiest way to increase readership by 146% in just 3 month, improve SEO and get links back!</h1>
-	<p style="font-size: 1.6em; margin: 1em 0px 0.5em;">To start using the plugin, please</p>
+    <h1>AtContent is the easiest way to make your blog available for reposting and increase your audience by 152%!</h1>
+	<p style="font-size: 1.6em; margin: 1em 0px 0.5em;">Start using AtContent in a few clicks</p>
 	<iframe id="ac_connect" src="http://atcontent.com/Auth/WordPressConnect/?ping_back=<?php echo $ajax_action ?>" style="width:302px;height:50px;" frameborder="0" scrolling="no"></iframe>
    <hr />
         
