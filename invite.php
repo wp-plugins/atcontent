274983eccb450789f78e7dc21bee4aacdc13974e
opuramu <opuramu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-31 21:01:43 +0000

    connect page update
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@849052 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- invite.php
+++ invite.php
@@ -89,7 +89,7 @@
     <?php if (strlen($ac_api_key) == 0)
             {     
           ?>  
-    <h1>Get quality posts for your site and boost readership 2,5x in 30 days!</h1>
+    <h1>Boost your audience 2.5x in 30 days by sharing blog posts with others!</h1>
 	<p id="connection_rules_title" style="font-size: 1.6em; font-weight: 300;">The connection will create an account on AtContent.com.</p>
         <div id="user_data_form">
             <input id="email" type="text" name="email" value="<?php echo $email?>"></input></br>
