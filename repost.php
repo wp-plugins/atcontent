b52590fb44b3cbfa62ceedf978337ebacb5dead2
opuramu <opuramu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-04 10:06:46 +0000

    remove test widget
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/branches/dev-createcampaignlinks@887421 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- repost.php
+++ repost.php
@@ -241,15 +241,6 @@ $email_body = "Hey AtContent team, \n" .
                 </div>
             <?php } ?>
         </div>
-<div class="atcontent_widget">
-    <div class="atcontent_preview">
-        <p> Text text text Second paragraph Third paragraph Fourth paragraph. Long long long text. </p>
-    </div>
-    <script src="https://w.atcontent.com/-/3eXrDmoH-6z/331/0HeZK8Vz19y.text/Title/h3"></script>
-    <script data-ac-src="https://w.atcontent.com/-/3eXrDmoH-6z/331/0HeZK8Vz19y.text/Face"></script>
-    <!--more-->
-    <script data-ac-src="https://w.atcontent.com/-/3eXrDmoH-6z/331/0HeZK8Vz19y.text/Body"></script>
-</div>
 <script>
     (function ($) {
         $(function () {
