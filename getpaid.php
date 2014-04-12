243e17ec76e5fc5b84c342982fb06ea5bb6b4f27
ariser <ariser@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-12 21:36:24 +0000

    code formatting cleanup
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@892050 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- getpaid.php
+++ getpaid.php
@@ -1,6 +1,6 @@
 <div class="atcontent_wrap get_paid_message_block">
-        <h1>You are on the way to our native ad marketplace, please wait</h1>
-        <script type="text/javascript">
-              location.href = 'http://bit.ly/1j7vVbM';
-        </script>
-   </div>
+    <h1>You are on the way to our native ad marketplace, please wait</h1>
+    <script type="text/javascript">
+          location.href = 'http://bit.ly/1j7vVbM';
+    </script>
+</div>
