b41d23c1452e95bdaf1ddfe5dd2744834d35010a
Berd <Berd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-29 21:24:34 +0000

    minor code improvements
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@905075 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/atcontent_coexistense.php
+++ include/atcontent_coexistense.php
@@ -9,14 +9,20 @@ function atcontent_coexistense_pin_it_buttons_add( $content ) {
 	//$pin_button    = '<span class="pinit-button-'.$pin_count.'"><a href="http://pinterest.com/pin/create/button/?url=%s&media=%s&description=%s" class="pin-it-button" count-layout="%s"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a></span>';
 	// The shortcodes run after the filter. So we'll check for them in $content instead.
 	$pinit = $options['pinit_pinit'];
-	if ( strpos($content, '[nopinit]') )
+	if ( strpos($content, '[nopinit]') ) {
 			$pinit = false;
-	if ( strpos($content, '[dopinit]') )
+	}
+	
+	if ( strpos($content, '[dopinit]') ) {
 			$pinit = true;
+	}
+	
 	// First see if we want to show pins according to the settings.
 	$options = get_option('pin_it_buttons_options');
-	if ( $pinit==false ) 
-					return $content;
+
+	if ( $pinit==false ) {
+			return $content;
+	}
 
 	// Tag all images
 	$html = str_get_html( $content, true, true, DEFAULT_TARGET_CHARSET, false );     
