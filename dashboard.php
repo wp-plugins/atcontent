aea5f8913efa7c6ee45280d86785d79f2003ab79
vadnov <vadnov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-21 06:16:58 +0000

    text updates
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@935988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dashboard.php
+++ dashboard.php
@@ -24,7 +24,7 @@
     <?php
         // PingBack
         if ( ! atcontent_pingback_inline() ) {
-            echo "<div class=\"error\">" . 'Could not connect to the <a href="http://atcontent.com" target=_blank>AtContent</a> server. Contact your hosting provider.' . "</div>";
+            echo "<div class=\"error\">" . 'Could not connect to the <a href="http://atcontent.com" target=_blank>AtContent.com</a> server. Please, contact your hosting provider to solve this issue.' . "</div>";
         }
         //End PingBack
         include("include/atcontent_settings.php");
