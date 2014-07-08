85aef2b19d3b65715461528f50c010a4b0ccc307
vadnov <vadnov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-08 14:38:30 +0000

    url fix
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@944988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stat_block.php
+++ stat_block.php
@@ -10,7 +10,7 @@
             <ol style="font-size: 1.2em;">
                 <li>Get relevant posts from “<a href="<?php echo admin_url( "admin.php?page=atcontent/repost.php" ); ?>">Content Feed</a>” page.<br>
                 <small>Readers will come to your blog more often to read brand new content and share it on social networks!</small></li>
-                <li>Set tags and follow relevant bloggers in <br> your <a href="https://atcontent.com/studio/statistics?wp=1#tips" target="_blank">AtContent dashboard</a>.<br>
+                <li>Set tags and follow relevant bloggers in <br> your <a href="http://atcontent.com/studio/statistics?wp=1#tips" target="_blank">AtContent dashboard</a>.<br>
                 <small>You will get a perfect chance on repost and reach a much wider audience.</small></li>
             </ol>
             <p>
