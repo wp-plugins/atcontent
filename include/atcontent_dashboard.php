035132f58b8985fa21fc4e9132ae872b33f8703f
vadnov <vadnov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-10 06:54:01 +0000

    texts update
    git-svn-id: https://plugins.svn.wordpress.org/atcontent/trunk@946018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/atcontent_dashboard.php
+++ include/atcontent_dashboard.php
@@ -22,34 +22,34 @@ function atcontent_dashboard_widget_function() {
             }, function(r) {
                 if (r && r.IsOK) {
                     var html = '';
-                    html += '<div class="ac-dashboard-brief"><table><tr><th><a id="ac-dashboard-monthly-stats" href="#">Monthly Stats</a></th>' + 
-                            '<th class="ac-dashboard-brief__tab-inactive"><a id="ac-dashboard-cumulative-stats" href="#">Cumulative Stats</a></th></tr>';
+                    html += '<div class="ac-dashboard-brief"><table><tr><th><a id="ac-dashboard-monthly-stats" href="#">Last ' + (r.days > 1 ? r.days + ' Days' : 'Day') + '</a></th>' + 
+                            '<th class="ac-dashboard-brief__tab-inactive"><a id="ac-dashboard-cumulative-stats" href="#">All Time</a></th></tr>';
                     html += '<tr id="ac-dashboard-monthly-row">' +                             
                             '<td><div class="b-dashboard-brief__left"><div class="b-dashboard-brief__value b-dashboard-brief__value_orange">' + r.monthlyBlogReposts + '</div>' + 
                             '<div class="b-dashboard-brief__description">repost' + (r.monthlyBlogReposts == 1 ? '' : 's' ) + ' of your content' + 
-                            '<br>for the last ' + r.days + ' ' + (r.days > 1 ? 'days' : 'day') + '</div>' + 
+                            '<br>by other bloggers</div>' + 
                             '<div class="b-dashboard-brief__value b-dashboard-brief__value_small b-dashboard-brief__value_blue">' + r.monthlyBlogRepostViews + '</div>' + 
-                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views</div>' + 
+                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views across AtContent network</div>' + 
                             '</div></td>' + 
                             '<td><div class="b-dashboard-brief__left"><div class="b-dashboard-brief__value b-dashboard-brief__value_orange">' + r.monthlyReposts + '</div>' + 
-                            '<div class="b-dashboard-brief__description">repost' + (r.monthlyReposts == 1 ? '' : 's' ) + ' on your blog' + 
-                            '<br>for the last ' + r.days + ' ' + (r.days > 1 ? 'days' : 'day') + '</div>' + 
+                            '<div class="b-dashboard-brief__description">repost' + (r.monthlyReposts == 1 ? '' : 's' ) + ' you\'ve made' + 
+                            '<br>&nbsp;</div>' + 
                             '<div class="b-dashboard-brief__value b-dashboard-brief__value_small b-dashboard-brief__value_blue">' + r.monthlyRepostViews + '</div>' +
-                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views</div>' +
+                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views of reposts on your blog</div>' +
                             '</div></td>' + 
                             '</tr>';
                     html += '<tr id="ac-dashboard-cumulative-row">' + 
                             '<td><div class="b-dashboard-brief__left"><div class="b-dashboard-brief__value b-dashboard-brief__value_orange">' + r.totalBlogReposts + '</div>' + 
                             '<div class="b-dashboard-brief__description">repost' + (r.totalBlogReposts == 1 ? '' : 's' ) + ' of your content' + 
-                            '<br>since you joined AtContent</div>' + 
+                            '<br>by other bloggers</div>' + 
                             '<div class="b-dashboard-brief__value b-dashboard-brief__value_small b-dashboard-brief__value_blue">' + r.totalBlogRepostViews + '</div>' + 
-                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views</div>' + 
+                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views across AtContent network</div>' + 
                             '</div></td>' + 
                             '<td><div class="b-dashboard-brief__left"><div class="b-dashboard-brief__value b-dashboard-brief__value_orange">' + r.totalReposts + '</div>' + 
-                            '<div class="b-dashboard-brief__description">repost' + (r.totalReposts == 1 ? '' : 's' ) + ' on your blog' + 
-                            '<br>since you joined AtContent</div>' + 
+                            '<div class="b-dashboard-brief__description">repost' + (r.totalReposts == 1 ? '' : 's' ) + ' you\'ve made' + 
+                            '<br>&nbsp;</div>' + 
                             '<div class="b-dashboard-brief__value b-dashboard-brief__value_small b-dashboard-brief__value_blue">' + r.totalRepostViews + '</div>' +
-                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views</div>' +
+                            '<div class="b-dashboard-brief__description b-dashboard-brief__description_small">views of reposts on your blog</div>' +
                             '</div></td>' + 
                             '</tr>';
                     
@@ -88,7 +88,7 @@ function atcontent_add_dashboard_widgets() {
     $userid = wp_get_current_user()->ID;
     if ( !user_can( $userid, "edit_posts" ) ) return;
     $img_url = plugins_url( 'assets/logo.png', dirname( __FILE__ ) );
-    wp_add_dashboard_widget('atcontent_dashboard_widget', '<img style="vertical-align:middle;" src="' . $img_url . '" alt=""> AtContent', 'atcontent_dashboard_widget_function');
+    wp_add_dashboard_widget('atcontent_dashboard_widget', '<img style="vertical-align:middle;" src="' . $img_url . '" alt=""> AtContent Stats', 'atcontent_dashboard_widget_function');
     global $wp_meta_boxes;
     $normal_dashboard = $wp_meta_boxes['dashboard']['normal']['core'];
     $atcontent_dashboard_widget = array('atcontent_dashboard_widget' => $normal_dashboard['atcontent_dashboard_widget']);
